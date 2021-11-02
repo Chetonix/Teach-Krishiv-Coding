2b5d8955e6ac40835ce86b8b0b44bc4199050cf7
Chetan Harjani <chetonix@gmail.com>
2021-11-02 16:17:12 +0530

    Add score to the code

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,9 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
+var yourChoice;
+var compChoice;
+var score = 0;
+
 while (yourChoice != "E") {
   var yourChoice = prompt(
     "Type 0 for: Rock, 1 for: Paper, 2 for: Scissors, & E for: Exit- "
@@ -12,26 +16,34 @@ while (yourChoice != "E") {
   }
   if (yourChoice == 0 && compChoice == 1) {
     console.log("Rock Vs Paper: You lose!");
+    score--;
   }
   if (yourChoice == 0 && compChoice == 2) {
     console.log("Rock Vs Scissors: You win!");
+    score++;
   }
   if (yourChoice == 1 && compChoice == 0) {
     console.log("Paper Vs Rock: You win!");
+    score++;
   }
   if (yourChoice == 1 && compChoice == 1) {
     console.log("Paper vs Paper: Game Draw!");
   }
   if (yourChoice == 1 && compChoice == 2) {
     console.log("Paper Vs Scissors: You lose!");
+    score--;
   }
   if (yourChoice == 2 && compChoice == 0) {
     console.log("Scissors Vs Rock: You lose!");
+    score--;
   }
   if (yourChoice == 2 && compChoice == 1) {
     console.log("Scissors Vs Paper: You win!");
+    score++;
   }
   if (yourChoice == 2 && compChoice == 2) {
     console.log("Scissors Vs Scissors: Game Draw!");
   }
 }
+
+console.log("Score: ", score);
